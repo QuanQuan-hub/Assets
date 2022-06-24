1610ec8fef03075fd599db5e305b20e8ca50d854
ZhangQuan <zq05189598@qq.com>
2022-06-24 22:46:02 +0800

    ...

---
+++ Scripts/Test/testScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.AddListener(EventID.test, DebugABanana);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            EventManager.ExecuteEvent(EventID.test);
+        }
+    }
+
+    private void DebugABanana()
+    {
+        print("banana");
+    }
+}
