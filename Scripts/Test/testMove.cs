dcb8821fa3a0323eed8bc267aad7d537b586c39d
ZhangQuan <zq05189598@qq.com>
2022-06-27 01:51:13 +0800

    【模型动作】创建了状态机、动画混合树

---
+++ Scripts/Test/testMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testMove : MonoBehaviour
+{
+    private Animator animator;
+    private float x;
+    private float y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        if (InputManager.Instance.IsKeyHold(ActionEnum.jump ))
+        {
+            x *= 2;
+            y *= 2;
+        }
+        animator.SetBool("isCrouch", InputManager.Instance.IsKeyHold(ActionEnum.attack));
+        animator.SetFloat("rightVector", x);
+        animator.SetFloat("forwardVector", y);
+    }
+}
